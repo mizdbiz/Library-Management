409f781f02540e0e9054726bd2413c457b46e762
jisha-maria <jisha.maria@dbizsolution.com>
2025-07-08 16:36:22 +0530

    changed functions to class

--- apps/Student-Service/src/controllers/student.controller.ts
+++ apps/Student-Service/src/controllers/student.controller.ts
@@ -1,24 +1,28 @@
 import { Request, Response } from "express";
-import { getAllStudents, createstudent, updatestudent, deleteStudent } from "../services/student.service";
+import { Studentservice } from "../services/student.service";
+import { StudentRepository } from "../repositories/student.repository";
   
 
+const StudentService = new Studentservice(new StudentRepository());
+
 export const getStudents = async(req: Request, res: Response)=> {
-    res.json(await getAllStudents());
+    res.json(await StudentService.getAllStudents());
 };
 
 export const createStudentController = async(req: Request, res: Response) => {
-    const student = await createstudent(req.body);
+    const student = await StudentService.createstudent(req.body);
+    await StudentService.save(student);
     res.status(201).json(student);
 };
 
 export const updateStudentController = async(req: Request, res: Response) => {
     const id = Number(req.params.id);
-    const updated = await updatestudent(id, req.body);      
+    const updated = await StudentService.updatestudent(id, req.body);      
     res.json(updated);
 };
 
 export const deleteStudentController = async(req: Request, res: Response) => {
     const id = Number(req.params.id);
-    const result = await deleteStudent(id);
+    const result = await StudentService.deletestudent(id);
     res.json(result);
 };
