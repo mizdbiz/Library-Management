bcd10bfeb0263d65f24dfb9b23526ffc014da1e0
jisha-maria <jisha.maria@dbizsolution.com>
2025-07-10 17:15:46 +0530

    error handler

--- apps/Student-Service/src/controllers/student.controller.ts
+++ apps/Student-Service/src/controllers/student.controller.ts
@@ -1,28 +1,49 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { StudentService } from "../services/student.service";
 import { StudentRepository } from "../repositories/student.repository";
   
 
 const studentservice = new StudentService(new StudentRepository());
 
-export const getStudents = async(req: Request, res: Response)=> {
-    res.json(await studentservice.getAllStudents());
+export const getStudents = async(req: Request, res: Response, next: NextFunction)=> {
+    try {
+        const students = await studentservice.getAllStudents();
+        res.json(students);
+    } catch (error) {
+        next(error); //throw
+    }
 };
 
-export const createStudentController = async(req: Request, res: Response) => {
-    const student = await studentservice.createstudent(req.body);
-    await studentservice.save(student);
-    res.status(201).json(student);
+export const createStudentController = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const student = studentservice.createstudent(req.body);
+        await studentservice.save(student);
+        res.status(201).json(student);
+    } catch (error) {
+        next(error);
+    }
 };
 
-export const updateStudentController = async(req: Request, res: Response) => {
-    const id = Number(req.params.id);
-    const updated = await studentservice.updatestudent(id, req.body);      
-    res.json(updated);
+export const updateStudentController = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = Number(req.params.id);
+        const updated = await studentservice.updatestudent(id, req.body);      
+        if (updated)
+            {res.json(updated);}
+        else {
+            throw new Error("Student not found");
+        }
+    } catch (error) {
+        next(error);
+    }
 };
 
-export const deleteStudentController = async(req: Request, res: Response) => {
-    const id = Number(req.params.id);
-    const result = await studentservice.deletestudent(id);
-    res.json(result);
+export const deleteStudentController = async(req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = Number(req.params.id);
+        const result = await studentservice.deletestudent(id);
+        res.json(result);
+    } catch (error) {
+        next(error);
+    }
 };
