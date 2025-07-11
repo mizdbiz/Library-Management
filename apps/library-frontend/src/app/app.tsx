cba8e8078b65848fd6666ea6938885e0cb560585
jisha-maria <jisha.maria@dbizsolution.com>
2025-07-11 14:34:15 +0530

    reactapp setup

---
+++ apps/library-frontend/src/app/app.tsx
@@ -0,0 +1,13 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@library-management/library-frontend" />
+    </div>
+  );
+}
+
+export default App;
